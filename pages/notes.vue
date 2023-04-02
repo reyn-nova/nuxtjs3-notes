9b6989f86d403c49f96cb07ab758a31347d4af78
nald-dev <rynvva@gmail.com>
2023-04-02 16:06:34 +0700

    initial commit

---
+++ pages/notes.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <h1>
+            Notes Page
+        </h1>
+    </div>
+  </template>
+  
+  <style scoped>
+    body {
+      margin: 0;
+    }
+
+    .container {
+        height: 100vh;
+        width: 100vw;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+  </style>
+  
